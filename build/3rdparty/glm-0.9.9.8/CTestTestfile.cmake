08fe0b63bc0bac8f3a1da65953972798d64d6103
Santiego <2421653893@qq.com>
2023-04-08 10:46:42 +0800

    ignore files

--- build/3rdparty/glm-0.9.9.8/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/santiego/proj/CG/rockcore/cg_solve/3rdparty/glm-0.9.9.8
-# Build directory: /home/santiego/proj/CG/rockcore/cg_solve/build/3rdparty/glm-0.9.9.8
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("glm")
--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/santiego/proj/CG/rockcore/cg_solve
-# Build directory: /home/santiego/proj/CG/rockcore/cg_solve/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("3rdparty/glad")
-subdirs("3rdparty/glm-0.9.9.8")
